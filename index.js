a4355513541d669a1024f4eed8eb4bf74aa5ec3e
MARLEY CLYFF BONIFACIO DA SILVA <marley_c_silva@estudante.sesisenai.org.br>
2024-07-04 17:35:53 -0300

    db

--- index.js
+++ index.js
@@ -1,3 +1,49 @@
+const express = require('express')
+const { Client } = require('pg')
+require("dotenv").config()
+
+const client = new Client({
+    host: process.env.host,
+    port: process.env.port,
+    user: process.env.user,
+    password: process.env.password,
+    database: process.env.database
+})
+
+const app = express()
+
+const connectDB = async () => {
+    client
+    .connect()
+    .then(() => {
+        console.log('a conexão funcionou');
+    }).catch((err) => {
+        console.log('erro ao conectar na db');
+    });
+};
+connectDB()
+
+app.get('/test-api', function (req,res){
+    res.send(' CALIU ')
+})
+
+app.listen(8000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const teste = [1,2,3,4,5,6]
 
 function filtrarPares(arr) {
